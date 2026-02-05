088270460a16df9517daaabaac8acb5ee47454ad
Drnggp <94812977+DioRangga@users.noreply.github.com>
2026-02-05 16:12:12 +0700

    navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,24 @@ function App() {
 
   return (
     <>
-    <h1 className="text-4xl text-red-600 font-bold">Hello World</h1>
+    <h1 className="text-3xl">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit eligendi praesentium 
+      minima ea molestias assumenda facilis nisi autem repellat eos temporibus distinctio, est 
+      debitis quam culpa cumque illo voluptates! Reiciendis assumenda quasi illum velit, ea corrupti 
+      aspernatur similique. Quos nemo soluta, exercitationem recusandae corporis sunt dicta atque e
+      xpedita ab asperiores eius, doloremque saepe totam, eaque perferendis? Veniam architecto 
+      nostrum est. Cumque, soluta unde, nihil totam maxime facilis quod dolores perspiciatis f
+      acere ratione eum harum eaque incidunt minima quia impedit repudiandae? Voluptates alias 
+      aspernatur expedita explicabo, enim rerum facere, earum tempore, cumque libero in ullam 
+      nihil corporis sequi omnis aperiam recusandae odit commodi! Quod temporibus numquam aut optio 
+      voluptatibus sint amet pariatur id autem a unde doloremque, error eaque dolorum possimus. C
+      onsequatur, vero sint quibusdam, dolor neque perspiciatis repellendus exercitationem totam 
+      aliquid, error ullam ratione nihil impedit. Quia deserunt delectus tenetur! Ratione possimus n
+      ecessitatibus tenetur vitae atque nesciunt? Deleniti voluptates esse in expedita minima tenetur 
+      vero perspiciatis sit? Quisquam, eligendi id soluta impedit voluptatum saepe quas perferendis
+       mollitia nulla harum! Cum fugit aut, dolor excepturi harum itaque unde tempore commodi aliquam.
+        Atque illo incidunt velit deleniti aliquid nemo quo sequi molestias, quae repudiandae 
+        quibusdam omnis ducimus pariatur hic! Incidunt, repellat numquam.</h1>
     </>
   )
 }
