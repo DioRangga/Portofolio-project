8af74c00c62eef8476a8ae0ae572bdd2b83a95f3
Drnggp <94812977+DioRangga@users.noreply.github.com>
2026-02-05 20:33:15 +0700

    1.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ function App() {
     <div>
       <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
         <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" />
-        <q>Kode Yang. Indah Lahir Dari Ketekunan😜 </q>
+        <q>Something To Jump 😜 </q>
       </div>
       <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Dio Rangga</h1>
-      <p>Saya saat ini berkerja sebagai IT Support disebuah lembaga pendidikan Internasional hampir 
-        5 tahun. Saya mempunyai ketertarikan dalam bidang network engineer dan cybersecurity,terutama 
+      <p className="">
+        Saya saat ini bekerja sebagai IT Support disebuah lembaga pendidikan Internasional hampir 
+        6 tahun. Saya mempunyai ketertarikan dalam bidang network engineer dan cybersecurity,terutama 
         pada keamanan sebuah jaringan atau sistem dalam lembaga ketertarikan ini sudah berlangsung 
         lebih dari satu tahun.</p>
     </div>
