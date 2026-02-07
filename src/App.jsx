1b744fdc9e9efd1fea5df4c7292b571d3f444b0c
Drnggp <94812977+DioRangga@users.noreply.github.com>
2026-02-08 00:13:30 +0700

    1.0.1
    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import DataImage from "./data";
-
+import { listTools } from "./data";
 
 function App() {
 
@@ -33,7 +33,8 @@ function App() {
 
     {/* tentang */}
       <div className="tentang mt-32 py-10">
-        <div className="w-2/3 mx-auto p-7 bg-zinc-800 rounded-lg">
+        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+        <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" />
           <p className="text-base/loose mb-10">
           Hi, perkenalkan saya Dio Rangga seorang IT Support dan CyberSecurity untuk keamanan suatu 
           jaringan di sebuah lembaga, saya percaya bahwa keamanan bukan sekadar fitur, melainkan keharusan. 
@@ -42,7 +43,7 @@ function App() {
           bagi seluruh pengguna.    
           </p>
           <div className="flex items-center justify-between">
-            <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md" />
+            <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md sm:block hidden" />
             <div className="flex items-center gap-6">
               <div>
                 <h1 className="text-4xl mb-1">
@@ -57,6 +58,28 @@ function App() {
                 <p>Tahun Pengalaman</p>
               </div>
             </div>
+          </div>
+        </div>
+
+        <div className="tools mt-32">
+        <h1 className="text-4xl/snug font-bold mb-4">Tools yang dipakai</h1>
+        <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-fulltext-base/loose 
+        opacity-50">Berikut ini beberapa tools yang biasa saya 
+        pakai untuk pembuatan website ataupun Design</p>
+          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2
+          grid-cols-1 gap-4 ">
+
+            {listTools.map((tool) => (
+              <div className=" flex items-center gap-2 p-3 border border-zinc-600 rounded-md
+                   hover:bg-zinc-800 group key={tool.id}>">
+                <img src={tool.gambar} alt="Tools Image" className="w-14 bg-zinc-800 p-1 
+                   group-hover:bg-zinc-900" />
+              <div>
+                <h4 className="font-bold"> {tool.nama}</h4>
+                <p className=" opacity-50">{tool.ket}</p>
+              </div>
+               </div>
+            ))}
           </div>
         </div>
       </div> 
